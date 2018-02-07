665ff0814034340e24a5252a61c2cf021f66ce2f
anatoly <anatoly@stratim.com>
2018-02-07 13:46:38 +0300

    Remove DiophantineEquationSolver

--- src/test/java/ru/geneticalgorithms/example/DiophantineEquation.java
+++ src/test/java/ru/geneticalgorithms/example/DiophantineEquation.java
@@ -1,5 +1,7 @@
 package ru.geneticalgorithms.example;
 
+import org.junit.Before;
+import org.junit.Test;
 import ru.geneticalgorithms.core.GeneticAlgorithm;
 import ru.geneticalgorithms.core.function.FitnessFunction;
 import ru.geneticalgorithms.core.function.GeneGenerator;
@@ -8,27 +10,23 @@ import ru.geneticalgorithms.core.function.mutation.RandomMutationFunction;
 import ru.geneticalgorithms.core.function.parentselect.RouletteWheelParentSelectFunction;
 import ru.geneticalgorithms.core.function.terminatecondition.DefaultTerminateCondition;
 import ru.geneticalgorithms.core.model.Gene;
-import ru.geneticalgorithms.core.model.Individual;
 
 import java.util.Random;
 
 /**
  * @author avnik
  */
-public class DiophantineEquationSolver {
+public class DiophantineEquation {
   private static final Random RANDOM = new Random();
+
+  private GeneticAlgorithm.Builder builder;
+
   private int result;
   private int[] coefficients;
-  private final GeneticAlgorithm<Integer> geneticAlgorithm;
 
-  public DiophantineEquationSolver(int result, int... coefficients) {
-    this.result = result;
-    this.coefficients = coefficients;
-    this.geneticAlgorithm = new GeneticAlgorithm.Builder()
-        .setChromosomeLength(coefficients.length)
-        .setPopulationSize(50)
-        .setMutationRate(0.1)
-        .setCrossoverRate(0.95)
+  @Before
+  public void setup() {
+    this.builder = new GeneticAlgorithm.Builder()
         .setElitismCount(1)
         .setMaxGenerationsCount(100)
         .setFitnessFunction(fitnessFunction)
@@ -36,15 +34,32 @@ public class DiophantineEquationSolver {
         .setTerminateCondition(new DefaultTerminateCondition<>())
         .setParentSelectFunction(new RouletteWheelParentSelectFunction<>())
         .setCrossoverFunction(new RandomCrossoverFunction<>())
-        .setMutationFunction(new RandomMutationFunction<>(geneGenerator))
+        .setMutationFunction(new RandomMutationFunction<>(geneGenerator));
+  }
+
+  @Test
+  public void testSolverWith3Roots() {
+    this.result = 20;
+    this.coefficients = new int[]{3, 5, 2};
+
+    newGeneticAlgorithm().run();
+  }
+
+  @Test
+  public void testSolverWith10Roots() {
+    this.result = 81;
+    this.coefficients = new int[]{13, 8, 7, -5, -3, 6, 1, -1, 20, -3};
+
+    newGeneticAlgorithm().run();
+  }
+
+  private GeneticAlgorithm<Integer> newGeneticAlgorithm() {
+    return this.builder
+        .setChromosomeLength(coefficients.length)
         .build();
   }
 
-  public Individual<Integer> run() {
-    return geneticAlgorithm.run();
-  }
-
-  private FitnessFunction<Integer> fitnessFunction = chromosome -> {
+  private final FitnessFunction<Integer> fitnessFunction = chromosome -> {
     int result = 0;
     for (int i = 0; i < coefficients.length; i++) {
       result += coefficients[i] * chromosome.get(i).getValue();
@@ -54,5 +69,5 @@ public class DiophantineEquationSolver {
     return 1 / Math.exp(difference);
   };
 
-  private GeneGenerator<Integer> geneGenerator = previousGenes -> new Gene<>(1 + RANDOM.nextInt(result));
+  private final GeneGenerator<Integer> geneGenerator = previousGenes -> new Gene<>(1 + RANDOM.nextInt(result));
 }
